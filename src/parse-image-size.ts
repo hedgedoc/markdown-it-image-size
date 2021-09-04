a10a13f1de66672fb66fa2caef1b413232873c3a
Tilman Vatteroth <git@tilmanvatteroth.de>
2021-09-04 19:45:00 +0200

    Fix license issues

--- src/parse-image-size.ts
+++ src/parse-image-size.ts
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 import { SpecialCharacters } from './specialCharacters'
 
 export interface ParseImageSize {
