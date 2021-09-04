cd708b1c86a5ca4fd0641c583cda7923bdd8c74a
Tilman Vatteroth <git@tilmanvatteroth.de>
2021-09-04 19:28:00 +0200

    Init with typescript convert

---
+++ src/specialCharacters.ts
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+export enum SpecialCharacters {
+  EXCLAMATION_MARK = 0x21,
+  OPENING_BRACKET = 0x5B,
+  OPENING_PARENTHESIS = 0x28,
+  WHITESPACE = 0x20,
+  NEW_LINE = 0x0A,
+  EQUALS = 0x3d,
+  LOWER_CASE_X = 0x78,
+  NUMBER_ZERO=0x30,
+  NUMBER_NINE=0x39,
+  PERCENTAGE=0x25
+}
